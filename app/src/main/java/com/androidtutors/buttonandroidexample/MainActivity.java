b668f2b0f08b293b5607fd9bd916a4eea43fb65e
Megha <compchap.megha@gmail.com>
2018-02-26 16:15:22 -0800

    Adding TODO's

--- app/src/main/java/com/androidtutors/buttonandroidexample/MainActivity.java
+++ app/src/main/java/com/androidtutors/buttonandroidexample/MainActivity.java
@@ -5,9 +5,18 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //TODO 7 : Add TextView variable with name helloTextView
+
+    //TODO 8: Add Button variable with name clickButton
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //TODO 9 Use findViewById to link helloTextView to TextView textview-hello
+
+        //TODO 10 Use findViewById to link clickButton to Button button_example
+
     }
 }
