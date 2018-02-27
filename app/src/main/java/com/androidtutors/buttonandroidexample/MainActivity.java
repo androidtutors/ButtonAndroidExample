4afffa75e913850d208ba23e565cd06b480e7543
Megha <compchap.megha@gmail.com>
2018-02-26 16:20:33 -0800

    //TODO 7 : Add TextView variable with name helloTextView //TODO 8: Add Button variable with name clickButton

--- app/src/main/java/com/androidtutors/buttonandroidexample/MainActivity.java
+++ app/src/main/java/com/androidtutors/buttonandroidexample/MainActivity.java
@@ -2,12 +2,16 @@ package com.androidtutors.buttonandroidexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     //TODO 7 : Add TextView variable with name helloTextView
+    private TextView helloTextView;
 
     //TODO 8: Add Button variable with name clickButton
+    private Button clickButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
